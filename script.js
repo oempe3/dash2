f3ab9cfd5fca4478e3494d05f39bfb072c24dead
oempe3 <oem.pe3@gmail.com>
2025-06-29 05:03:40 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-document.querySelectorAll("details").forEach(detail => {
-    detail.addEventListener("toggle", () => {
-        if (detail.open) {
-            detail.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        }
-    });
-});
